7ba360fca96ce125e0ff288bf04f965542fa64b4
ZHONGHuanGit <wpzhonghuan@gmail.com>
2014-08-03 14:08:55 +0800

    composition

---
+++ 5组合复用原则/Inheritance.java
@@ -0,0 +1,28 @@
+class Person
+{
+	public Person(){}
+}
+
+class Teacher extends Person
+{
+	public Teacher()
+	{
+		System.out.println("I am a teacher.");
+	}
+}
+
+class Student extends Person
+{
+	public Student()
+	{
+		System.out.println("I am a student.");
+	}
+}
+
+class Athlete extends Person
+{
+	public Athlete()
+	{
+		System.out.println("I am a athlete");
+	}
+}
