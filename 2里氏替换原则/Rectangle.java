c7d3dff18bfb51cb39dded7fe7b38799c84d0f5e
ZHONGHuanGit <wpzhonghuan@gmail.com>
2014-08-08 21:56:11 +0800

    refresh code

--- 2里氏替换原则/Rectangle.java
+++ 2里氏替换原则/Rectangle.java
@@ -1,16 +1,7 @@
 //普通的矩形
 class Rectangle
 {
-	int length, width;
-	public int getLength()
-	{
-		return length;
-	}
-
-	public void setLength(int length)
-	{
-		this.length = length;
-	}
+	int height, width;
 
 	public int getWidth()
 	{
@@ -24,26 +15,21 @@ class Rectangle
 
 	public int getHight()
 	{
-		// TODO Auto-generated method stub
-		return 0;
+		return height;
+	}
+
+	public void setHeight(int height)
+	{
+		this.height = height;
 	}
 }
 
 /*
- * 因为Square的长宽相同，用一个size来统一
- * 所以改写了父类的方法，因为它们实际上都是对size进行修改
+ * 因为Square的长宽相同，用一个size来统一 所以改写了父类的方法，因为它们实际上都是对size进行修改
  */
-class Square extends Rectangle{
+class Square extends Rectangle
+{
 	int size;
-	public int getLength()
-	{
-		return size;
-	}
-
-	public void setLength(int length)
-	{
-		this.size = length;
-	}
 
 	public int getWidth()
 	{
@@ -53,6 +39,16 @@ class Square extends Rectangle{
 	public void setWidth(int width)
 	{
 		this.size = width;
+	}
+
+	public int getHight()
+	{
+		return size;
+	}
+
+	public void setHeight(int height)
+	{
+		this.size = height;
 	}
 
 	public int getSize()
@@ -66,32 +62,30 @@ class Square extends Rectangle{
 	}
 }
 
-public class Main{
-	
+public class Main
+{
+
 	/**
-	 * 这个方法主要作用就是调整长方形的长宽，让长方形的宽调整至比长更长。
+	 * 这个方法主要作用就是调整长方形的长宽，让长方形的width调整至比height更长。
 	 * 可是就是这样的方法，出问题了，因为如果穿进去的rec实际上是一个正方形的话，那么，程序会无限制的运行下去
 	 * 
 	 * @param rec
-	 * @return 
+	 * @return
 	 */
 	static void testRec(Rectangle rec)
 	{
-		while(rec.getWidth()<=rec.getHight())//
+		while (rec.getWidth() <= rec.getHight())//
 		{
-			rec.setWidth(rec.getWidth());
+			rec.setWidth(rec.getWidth()+1);
 		}
 	}
-	
+
 	public static void main(String[] args)
 	{
-		Rectangle rec=new Square();
+		Rectangle rec = new Square();
 		rec.setWidth(5);
-		rec.setLength(6);
-		testRec(rec);//就是这里出现问题了，这个方法会无限的运行下去，不信的话，你试试就ok了
-					
+		rec.setHeight(6);
+		testRec(rec);// 就是这里出现问题了，这个方法会无限的运行下去，不信的话，你试试就ok了
+
 	}
 }
-
-
-
